87b988d65ed279d242ffea9b2866253a293dd7f2
await-light <mailllj@163.com>
2023-01-13 15:09:42 +0800

    div and mod

--- relay/highprec.c
+++ relay/highprec.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-int abuf[3000], bbuf[3000], cbuf[3000];
+#define BUFSIZE 3000
 
 int char2int(char c)
 {
@@ -21,8 +21,63 @@ int int2char(int i)
 	return (i + 48);
 }
 
+void strn_mvleft(char str[], int n, char newstr[])
+{
+	/*
+	str: "123"
+	n: 3
+
+	new: "123000"
+	*/
+	strncpy(newstr, str, strlen(str));
+	for (int i = 0; i < n; i++)
+	{
+		strcat(newstr, "0");
+	}
+}
+
+int compare(char a[], char b[])
+{
+	/*
+	1  : a > b
+	-1 : a < b
+	0  : a = b
+	*/
+	int la, lb;
+	la = strlen(a); lb = strlen(b);
+
+	if (la > lb)
+	{
+		return 1;
+	}
+	if (la < lb)
+	{
+		return -1;
+	}
+	if (la == lb)
+	{
+		for (int i = 0; i <= la - 1; ++i)
+		{
+			if (char2int(a[i]) > char2int(b[i]))
+			{
+				return 1;
+			}
+			if (char2int(a[i]) < char2int(b[i]))
+			{
+				return -1;
+			}
+			if (char2int(a[i]) == char2int(b[i]) && i == la - 1)
+			{
+				return 0;
+			}
+		}
+	}
+}
+
 void add(char a[], char b[], char c[])
 {
+	int abuf[BUFSIZE], bbuf[BUFSIZE], cbuf[BUFSIZE];
+
 	int la, lb, lc, x;
 	la = strlen(a); lb = strlen(b);
 
@@ -53,13 +108,15 @@ void add(char a[], char b[], char c[])
 	}
 	c[lc + 1] = '\0';
 
-	memset(abuf, 0, 3000);
-	memset(bbuf, 0, 3000);
-	memset(cbuf, 0, 3000);
+	memset(abuf, 0, BUFSIZE);
+	memset(bbuf, 0, BUFSIZE);
+	memset(cbuf, 0, BUFSIZE);
 }
 
 void mul(char a[], char b[], char c[])
 {
+	int abuf[BUFSIZE], bbuf[BUFSIZE], cbuf[BUFSIZE];
+
 	int lena, lenb, lenc, x = 0;
 	
 	lena = strlen(a); lenb = strlen(b);
@@ -90,9 +147,9 @@ void mul(char a[], char b[], char c[])
 	}
 	c[lenc + 1] = '\0';
 
-	memset(abuf, 0, 3000);
-	memset(bbuf, 0, 3000);
-	memset(cbuf, 0, 3000);
+	memset(abuf, 0, BUFSIZE);
+	memset(bbuf, 0, BUFSIZE);
+	memset(cbuf, 0, BUFSIZE);
 }
 
 void sub(char a[], char b[], char c[])
@@ -100,6 +157,8 @@ void sub(char a[], char b[], char c[])
 	/*
 	a is bigger than b
 	*/
+	int abuf[BUFSIZE], bbuf[BUFSIZE], cbuf[BUFSIZE];
+
 	int la, lb, lc;
 	la = strlen(a); lb = strlen(b);
 	for (int i = 0; i <= la - 1; ++i)
@@ -130,7 +189,77 @@ void sub(char a[], char b[], char c[])
 	}
 	c[lc + 1] = '\0';
 
-	memset(abuf, 0, 3000);
-	memset(bbuf, 0, 3000);
-	memset(cbuf, 0, 3000);
+	memset(abuf, 0, BUFSIZE);
+	memset(bbuf, 0, BUFSIZE);
+	memset(cbuf, 0, BUFSIZE);
+}
+
+void div(char a[], char b[], char c[])
+{
+    char abuf[BUFSIZE], bbuf[BUFSIZE], cbuf[BUFSIZE];
+
+    int la, lb, lc;
+	la = strlen(a); lb = strlen(b);
+    lc = la - lb + 1;
+
+    // copy to buffer
+    strncpy(abuf, a, la);
+
+    for (int i = 0; i < lc; i++)
+    {
+    	memset(bbuf, 0, strlen(bbuf));
+    	strn_mvleft(b, lc - i - 1, bbuf);
+    	while (compare(abuf, bbuf) >= 0) // abuf >= bbuf
+        {
+        	cbuf[lc - 1 - i]++;
+        	// abuf -= bbuf;
+        	char r[BUFSIZE];
+        	sub(abuf, bbuf, r);
+        	memset(abuf, 0, BUFSIZE);
+        	strncpy(abuf, r, BUFSIZE);
+        }
+    }
+
+    while (cbuf[lc] == 0 && lc >= 1) lc--;
+
+   	for (int i = 0; i <= lc; i++)
+   	{
+   		c[i] = int2char(cbuf[lc - i]);
+   	}
+   	c[lc + 1] = '\0';
+
+   	memset(abuf, 0, BUFSIZE);
+	memset(bbuf, 0, BUFSIZE);
+	memset(cbuf, 0, BUFSIZE);
+}
+
+void mod(char a[], char b[], char c[])
+{
+    char abuf[BUFSIZE], bbuf[BUFSIZE];
+
+    int la, lb, lc;
+	la = strlen(a); lb = strlen(b);
+    lc = la - lb + 1;
+
+    // copy to buffer
+    strncpy(abuf, a, la);
+
+    for (int i = 0; i < lc; i++)
+    {
+    	memset(bbuf, 0, strlen(bbuf));
+    	strn_mvleft(b, lc - i - 1, bbuf);
+    	while (compare(abuf, bbuf) >= 0) // abuf >= bbuf
+        {
+        	// abuf -= bbuf;
+        	char r[BUFSIZE];
+        	sub(abuf, bbuf, r);
+        	memset(abuf, 0, BUFSIZE);
+        	strncpy(abuf, r, BUFSIZE);
+        }
+    }
+
+    strncpy(c, abuf, strlen(abuf));
+
+   	memset(abuf, 0, BUFSIZE);
+	memset(bbuf, 0, BUFSIZE);
 }
